5bfacca931d6e50383bf020e3f79da7327bcc837
Jack Hardy <jackhardy1@gmail.com>
2016-09-03 15:27:20 +0100

    player can attack

---
+++ game_logic/ship_methods.js
@@ -0,0 +1,55 @@
+function checkForShip (player,coordinates){
+  var shipPresent, ship;
+
+  for (var i=0;i< player.ships.length;i++) {
+    ship = player.ships[i];
+
+    shipPresent = ship.locations.filter(function (actualCoordinate){
+      return (actualCoordinate[0] === coordinates[0]) &&
+      (actualCoordinate[1] === coordinates[1]);
+    })[0];
+
+    if (shipPresent) {
+      return ship;
+    }
+  }
+  return false;
+}
+
+
+function damageShip(ship, coordinates){
+  ship.damage.push(coordinates);
+}
+
+function attack(player, coordinates) {
+  var ship = checkForShip(player, coordinates);
+
+  if (ship){
+      damageShip(ship, coordinates);
+    }
+}
+
+function checkForDamage(player, coordinates){
+  var damagePresent, ship;
+
+    for (var i=0;i< player.ships.length;i++) {
+      ship = player.ships[i];
+
+      damagePresent = ship.damage.filter(function (actualCoordinate){
+        return (actualCoordinate[0] === coordinates[0]) &&
+        (actualCoordinate[1] === coordinates[1]);
+      })[0];
+
+      if (damagePresent) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+
+module.exports.checkForShip = checkForShip;
+module.exports.damageShip = damageShip;
+module.exports.attack = attack;
+module.exports.checkForDamage = checkForDamage;
---
+++ test/main_test.js
@@ -0,0 +1,5 @@
+//Test suite
+
+describe('Mocha', function(){
+  
+});
